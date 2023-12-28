901661606ab833e375d5a5ea85e36e2fc7a7c7d9
Abdullah AlEzzo <abdullahalezzo@gmail.com>
2023-12-29 01:43:25 +0300

    Fix warning for array missing keys in `Box` page

--- app/(landing)/box/page.tsx
+++ app/(landing)/box/page.tsx
@@ -47,7 +47,7 @@ const Box = async () => {
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 mb-20'>
                         {
                             activeBoxes?.map(box => (
-                                <BoxCounter label={box.name} value1={box.cash} />
+                                <BoxCounter label={box.name} value1={box.cash} key={box.id} />
                             ))
                         }
                     </div>
