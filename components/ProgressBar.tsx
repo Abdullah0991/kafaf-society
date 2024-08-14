63814480ef1e7532edba86d6503c0572ac9c1a0f
Abdullah AlEzzo <abdullahalezzo@gmail.com>
2024-08-14 15:08:53 +0300

    fix: `ProgressBar` show `NaN` when max is 0

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -17,7 +17,7 @@ const ProgressBar = ({ max, current, label, className = '' }: ProgressBarProps) 
                 >
                     {label}
                     &nbsp;
-                    ({`${Math.round((current * 100) / max)} %`})
+                    ({`${max > 0 ? Math.round((current * 100) / max) : 0} %`})
                 </h4>
                 <span className="px-2 py-1 rounded-lg bg-lime-50 text-emerald-500 text-sm">
                     {currFormatter(current)} / <strong>{currFormatter(max)}</strong>
@@ -26,7 +26,7 @@ const ProgressBar = ({ max, current, label, className = '' }: ProgressBarProps) 
             <div className={`overflow-hidden bg-emerald-50 h-1.5 rounded-full w-full`}>
               <span
                   className="h-full bg-emerald-500 w-full block rounded-full"
-                  style={{ "width": `${Math.round((current * 100) / max)}%` }}
+                  style={{ "width": `${max > 0 ? Math.round((current * 100) / max) : 0}%` }}
               ></span>
             </div>
         </div>
